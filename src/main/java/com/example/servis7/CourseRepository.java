023b49daf39914b1f63428ad9d5fc387581903a9
black32h <blackspetya@gmail.com>
2025-01-12 11:10:43 +0200

    Update CourseRepository.java

--- src/main/java/com/example/servis7/CourseRepository.java
+++ src/main/java/com/example/servis7/CourseRepository.java
@@ -1,4 +1,8 @@
 package com.example.servis7;
 
-public class CourseRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Інтерфейс для роботи з базою даних через JPA
+public interface CourseRepository extends JpaRepository<Course, Long> {
+    // JpaRepository вже надає всі необхідні методи для CRUD операцій
 }
